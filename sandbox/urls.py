d1504fedaf2f891406913bd568c55c6f09afd954
Kalev Härk <kalevhark@gmail.com>
2019-06-29 11:22:54 +0300

    Lisatud feedback app

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -25,5 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Serve static files
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serve media files
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
