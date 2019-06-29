1d6bc29cf371339f2fde02c561cbe4ea1c61d0a6
Kalev Härk <kalevhark@gmail.com>
2019-06-29 12:02:00 +0300

    Lisatud feedback app

--- feedback/urls.py
+++ feedback/urls.py
@@ -6,13 +6,9 @@ from feedback import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="feedback/main.html"), name='feedback_main'),
-    path('list', views.FeedbackList.as_view(), name='feedback_list'),
-    path('create', views.FeedbackCreate.as_view(), name='feedback_create'),
+    path('list/', views.FeedbackList.as_view(), name='feedback_list'),
+    path('create/', views.FeedbackCreate.as_view(), name='feedback_create'),
     path('update/<int:pk>', views.FeedbackUpdate.as_view(), name='feedback_update'),
     path('delete/<int:pk>', views.FeedbackDelete.as_view(), name='feedback_delete'),
     path('<int:pk>', views.FeedbackDetail.as_view(), name='feedback_detail'),
 ]
-
-# if settings.DEBUG:
-#     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#     urlpatterns += staticfiles_urlpatterns()
