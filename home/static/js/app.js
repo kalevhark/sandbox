841f4f86c7ab8c79ab771e09d4055d6bbbf3c138
Kalev Härk <kalevhark@gmail.com>
2019-06-29 14:06:48 +0300

    Lisatud feedback app

--- home/static/js/app.js
+++ home/static/js/app.js
@@ -37,3 +37,16 @@ $.ajax({
     })
     }
 });
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
