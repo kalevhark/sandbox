7554f743e22177cc5c1c998b8adc370c61cd614a
Kalev Härk <kalevhark@gmail.com>
2019-06-29 11:49:07 +0300

    Lisatud feedback app

--- home/static/js/app.js
+++ home/static/js/app.js
@@ -1,3 +1,38 @@
-$(function () {
-    console.log("Hello!");
+$.ajax({
+    url:  '/feedback/list',
+    type:  'get',
+    dataType:  'json',
+    success: function  (data) {
+        let rows =  '';
+        data.feedbacks.forEach(feedback => {
+        rows += `
+        <tr>
+            <td>${feedback.feedback_name}</td>
+            <td>${feedback.feedback_contact}</td>
+            <td>${feedback.feedback_text}</td>
+            <td>
+                <button class="btn deleteBtn" data-id="${feedback.id}">Delete</button>
+                <button class="btn updateBtn" data-id="${feedback.id}">Update</button>
+            </td>
+        </tr>`;
+    });
+    $('[#myTable] > tbody').append(rows);
+    $('.deleteBtn').each((i, elm) => {
+        $(elm).on("click",  (e) => {
+            deleteFeedback($(elm))
+        })
+    })
+    }
 });
+
+function  deleteFeedback(el){
+    feedbackId  =  $(el).data('id')
+    $.ajax({
+        url:  `/feedback/delete/${feedbackId}`,
+        type:  'post',
+        dataType:  'json',
+        success:  function (data) {
+            $(el).parents()[1].remove()
+        }
+    });
+}
